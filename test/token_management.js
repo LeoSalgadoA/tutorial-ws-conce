512d7a661e9f7b0f5c16a1ef294209187e4bb652
LeoSalgadoA <vsalgado.a@gmail.com>
2018-10-11 02:51:47 -0300

    test

--- test/token_management.js
+++ test/token_management.js
@@ -1,7 +1,49 @@
-contract('token_management', function(accounts) {
-  it("should assert true", function(done) {
-    var token_management = token_management.deployed();
-    assert.isTrue(true);
-    done();
+const DAOServices = artifacts.require("./DAOServices.sol");
+const CasaWToken = artifacts.require("./CasaWToken.sol");
+const EtherlabToken = artifacts.require("./EtherlabToken.sol");
+
+const should = require("chai")
+  .use(require("chai-as-promised"))
+  .should();
+
+let sender;
+let casawAddress;
+let etherlabAddress;
+
+contract("token_management", async accounts => {
+  beforeEach(async () => {
+    sender = await DAOServices.new();
+    casaw = await CasaWToken.new();
+    etherlab = await EtherlabToken.new();
+    casawAddress = casaw.address;
+    etherlabAddress = etherlab.address;
+    await sender.addNewToken("CWT", casawAddress);
+    await sender.addNewToken("ELT", etherlabAddress);
+  });
+
+  it("should add new ELT token", async () => {
+    console.log("  ");
+    let address = await sender.tokens.call("ELT");
+    address.should.equal(etherlabAddress);
+  });
+
+  it("should add new CWT token", async () => {
+    console.log("  ");
+    let address = await sender.tokens.call("CWT");
+    address.should.equal(casawAddress);
+  });
+
+  it("should update supported token address", async () => {
+    console.log("  ");
+    await sender.addNewToken("ELT", casawAddress);
+    let address = await sender.tokens.call("ELT");
+    address.should.equal(casawAddress);
+  });
+
+  it("should remove unused supported token address ELT has CWT address we need to remove it", async () => {
+    console.log("  ");
+    await sender.removeToken("ELT");
+    let address = await sender.tokens.call("ELT");
+    address.should.equal("0x0000000000000000000000000000000000000000");
   });
 });
--- test/token_transfer.js
+++ test/token_transfer.js
@@ -1,7 +1,74 @@
-contract('token_transfer', function(accounts) {
-  it("should assert true", function(done) {
-    var token_transfer = token_transfer.deployed();
-    assert.isTrue(true);
-    done();
+const DAOServices = artifacts.require("./DAOServices.sol");
+const CasaWToken = artifacts.require("./CasaWToken.sol");
+const EtherlabToken = artifacts.require("./EtherlabToken.sol");
+
+const BigNumber = web3.BigNumber;
+
+const should = require("chai")
+  .use(require("chai-as-promised"))
+  .use(require("chai-bignumber")(BigNumber))
+  .should();
+
+let sender, casaw, etherlab;
+
+contract("token_transfer", async accounts => {
+  let [accountA, accountB] = accounts;
+
+  beforeEach(async () => {
+    sender = await DAOServices.new();
+    casaw = await CasaWToken.new();
+    etherlab = await EtherlabToken.new();
+
+    await sender.addNewToken("CWT", casaw.address);
+    await sender.addNewToken("ELT", etherlab.address);
+  });
+
+  it("should be able to transfer sender token to another wallet", async () => {
+    let amount = new BigNumber(100000);
+
+    await etherlab.approve(sender.address, amount, { from: accountA });
+    console.log(
+      "    ✓ user call etherlab token to approve dao services to transfer an amount of tokens for him"
+    );
+    let balance1 = (await etherlab.balanceOf(accountB)).toString();
+    console.log("    ✓ accountB balance1 : " + balance1);
+
+    await sender.transferTokens("ELT", accountB, amount, {
+      from: accountA
+    });
+    console.log(
+      "    ✓ dao service make ELT transfer from accountA to accountB"
+    );
+    let balance2 = (await etherlab.balanceOf(accountB)).toString();
+    console.log("    ✓ accountB balance2 : " + balance2);
+
+    balance2.should.equal(amount.toString());
+  });
+
+  it("should NOT be able to transfer sender token to another wallet", async () => {
+    let amount = new BigNumber(100000);
+    console.log("  ");
+
+    await etherlab.approve(sender.address, amount, { from: accountA });
+    console.log(
+      "    ✓ user call etherlab token to approve dao services to transfer an amount of tokens for him"
+    );
+
+    let balance1 = (await casaw.balanceOf(accountB)).toString();
+    console.log("    ✓ accountB balance1 : " + balance1);
+    console.log(
+      "    ✓ dao service make CWT instead of ELT transfer from accountA to accountB"
+    );
+    try {
+      await sender.transferTokens("CWT", accountB, amount, {
+        from: accountA
+      });
+    } catch (e) {
+      console.log("    ✓ revert cant transfer CTW");
+    }
+
+    let balance2 = (await casaw.balanceOf(accountB)).toString();
+    console.log("    ✓ accountB balance2 : " + balance2);
+    balance1.should.equal(balance2);
   });
 });
