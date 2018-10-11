c8678db7c4493eaeef623a60229b675e709b8199
LeoSalgadoA <vsalgado.a@gmail.com>
2018-10-11 01:51:40 -0300

    to step 3

---
+++ test/token_management.js
@@ -0,0 +1,7 @@
+contract('token_management', function(accounts) {
+  it("should assert true", function(done) {
+    var token_management = token_management.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
---
+++ test/token_transfer.js
@@ -0,0 +1,7 @@
+contract('token_transfer', function(accounts) {
+  it("should assert true", function(done) {
+    var token_transfer = token_transfer.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
